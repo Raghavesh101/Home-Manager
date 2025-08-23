27457e6cad4af42c83c5e8bd38540734eabe28ef
Raghavesh <98579324+Raghavesh101@users.noreply.github.com>
2025-08-23 11:02:07 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// This file holds your secret keys and is NOT uploaded to GitHub.
+window.firebaseConfig = {
+        apiKey: "AIzaSyASJe1f1-kM0Res8Di18Eu5HrlfvjB2oZo",
+        authDomain: "homemanager-4e00f.firebaseapp.com",
+        projectId: "homemanager-4e00f",
+        storageBucket: "homemanager-4e00f.firebasestorage.app",
+        messagingSenderId: "476624861696",
+        appId: "1:476624861696:web:65ea2ac4e80de5f9f976e5"
+};
